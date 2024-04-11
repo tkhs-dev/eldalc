356350458c34eba66f79194a8dff713932e6d99e
tkhs <tokuhausu@gmail.com>
2024-04-11 12:27:51 +0900

    リスニング,リーディングに対応

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -24,6 +24,30 @@ fun main() {
             )
         }
     }
+    val subCourses = setOf<SubCourse>(
+        SubCourse("リスニング","LI", "01"),
+        SubCourse("文法","GR","06"),
+        SubCourse("リーディング","RE","03"),
+        //SubCourse("テスト","JT","08"), //This course is not supported
+    )
+    println("Select a sub-course")
+    for ((index, s) in subCourses.withIndex()){
+        println("  ($index)${s.name}")
+    }
+    print(">")
+    val subCourseI = readlnOrNull()
+    if (subCourseI.isNullOrBlank()){
+        println("Error 1")
+        return
+    }
+    val subCourse = subCourseI.toIntOrNull()
+    if(subCourse == null){
+        println("Error 2")
+        return
+    }
+    val courseString = subCourses.elementAt(subCourse).courseString
+    val courseId = subCourses.elementAt(subCourse).courseId
+
     println("Enter a Unit number (ex: U001)")
     print(">")
     val unit = readlnOrNull()
@@ -33,7 +57,7 @@ fun main() {
     println("Select a step")
     val steps = unitInfo.steps?.filter { isCapableType(it?.type ?: "") }
     if(steps==null){
-        println("Error 1")
+        println("Error 3")
         return
     }
     for ((index, s) in steps.withIndex()){
@@ -42,19 +66,19 @@ fun main() {
     print(">")
     val step = readlnOrNull()
     if (step.isNullOrBlank()){
-        println("Error 2")
+        println("Error 4")
         return
     }
     val stepI = step.toIntOrNull()
     if(stepI == null){
-        println("Error 3")
+        println("Error 5")
         return
     }
 
     when(steps[stepI]?.type){
         "14" -> {
             val stepAns: AlcQuestion14Response = runBlocking{
-                client.get("https://nanext.alcnanext.jp/anetn/course/materials/ALC/TLP/TLP_GR/$unit/06/${steps[stepI]?.id}.json").body()
+                client.get(getBaseUrl(courseString,unit,courseId)+"${steps[stepI]?.id}.json").body()
             }
             for(q in stepAns.questions.orEmpty()){
                 if(q != null){
@@ -69,7 +93,7 @@ fun main() {
         }
         "15" -> {
             val stepAns: AlcQuestion15Response = runBlocking{
-                client.get("https://nanext.alcnanext.jp/anetn/course/materials/ALC/TLP/TLP_GR/$unit/06/${steps[stepI]?.id}.json").body()
+                client.get(getBaseUrl(courseString,unit,courseId)+"${steps[stepI]?.id}.json").body()
             }
             for(q in stepAns.questions.orEmpty()){
                 if(q != null){
@@ -86,7 +110,7 @@ fun main() {
         }
         "16" -> {
             val stepAns: AlcQuestion16Response = runBlocking{
-                client.get("https://nanext.alcnanext.jp/anetn/course/materials/ALC/TLP/TLP_GR/$unit/06/${steps[stepI]?.id}.json").body()
+                client.get(getBaseUrl(courseString,unit,courseId)+"${steps[stepI]?.id}.json").body()
             }
             for(q in stepAns.questions.orEmpty()){
                 if(q != null){
@@ -98,7 +122,7 @@ fun main() {
         }
         "20" -> {
             val stepAns: AlcQuestion20Response = runBlocking{
-                client.get("https://nanext.alcnanext.jp/anetn/course/materials/ALC/TLP/TLP_GR/$unit/06/${steps[stepI]?.id}.json").body()
+                client.get(getBaseUrl(courseString,unit,courseId)+"${steps[stepI]?.id}.json").body()
             }
             for(q in stepAns.questions.orEmpty()){
                 if(q != null){
@@ -114,7 +138,7 @@ fun main() {
             }
         }
         else -> {
-            println("Error 4")
+            println("Error 6")
             return
         }
     }
@@ -131,3 +155,8 @@ fun isCapableType(input: String): Boolean {
     val capable = setOf("14","15","16","20")
     return capable.contains(input)
 }
+
+fun getBaseUrl(courseString: String, unit: String?, courseId: String): String {
+    return "https://nanext.alcnanext.jp/anetn/course/materials/ALC/TLP/TLP_$courseString/$unit/$courseId/"
+}
+data class SubCourse(val name:String, val courseString:String, val courseId:String )
